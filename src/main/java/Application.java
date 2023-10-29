c310dfd64ebed079f71e3842e587ae74ea873a42
TamaraZolotovskaya <116792070+TamaraZolotovskaya@users.noreply.github.com>
2023-10-29 22:25:41 +0700

    Oms

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -15,7 +15,7 @@ public class Application {
         EmployeeDAO employeeDAO = new EmployeeDAOImpl();
         CityDAO cityDAO = new CityDAOimpl();
 
-        City tomsk = new City(4, "Omsk77+5");
+        City tomsk = new City(4, "Oms");
         Employee employeeNew = new Employee("John", "Smith", "male", 57, tomsk);
 
         Optional<Employee> employeeNewWithId = employeeDAO.create(employeeNew);
